c59c6f13678e79eacdf9412a8cf8ff8b32706d20
Thomas <Thomas.Robert.Tennoy@stud.hials.no>
2016-10-25 08:00:11 +0200

    article structure

---
+++ Server/Projectserver/src/main/java/no/ntnu/projectserver/Article.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.ntnu.projectserver;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Demyx-Laptop
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Article implements Serializable {
+    
+    @Id @GeneratedValue
+    Long articleId;
+    
+    String title;
+    String ingress;
+    String content;
+    String photoUrl;
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    Date datePosted = new Date();
+    
+    public Article() {
+    }
+
+    public Long getArticleId() {
+        return articleId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIngress() {
+        return ingress;
+    }
+
+    public void setIngress(String ingress) {
+        this.ingress = ingress;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public Date getDatePosted() {
+        return datePosted;
+    }  
+    
+}
