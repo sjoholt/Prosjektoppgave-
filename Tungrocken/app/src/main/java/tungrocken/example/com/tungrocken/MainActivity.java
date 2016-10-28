2999f79b7c294f5891158bd4eaf5218861133f00
sjoholt <sindre@sjoholt.net>
2016-10-28 13:19:52 +0200

    Ørliten skrivefeil

--- Tungrocken/app/src/main/java/tungrocken/example/com/tungrocken/MainActivity.java
+++ Tungrocken/app/src/main/java/tungrocken/example/com/tungrocken/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         // Håndtering av visning og klikk på hamburgermeny
-        hamburgerMenu hm = new hamburgerMenu();
+        HamburgerMenu hm = new HamburgerMenu();
         Intent i = hm.getHamburgerMenu(id, this.getApplicationContext());
         if(i != null) {
             startActivity(i);
