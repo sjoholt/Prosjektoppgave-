a739c36b7047fc222e792f1319bd8a0c38f0696c
Thomas <Thomas.Robert.Tennoy@stud.hials.no>
2016-10-28 10:34:18 +0200

    unused import

--- Server/Projectserver/src/main/java/no/ntnu/projectserver/MainClass.java
+++ Server/Projectserver/src/main/java/no/ntnu/projectserver/MainClass.java
@@ -8,7 +8,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
