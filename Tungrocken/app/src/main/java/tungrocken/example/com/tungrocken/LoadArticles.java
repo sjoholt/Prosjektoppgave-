02086aa1cbae399a7d797659ec889af4d44b105c
sjoholt <sindre@sjoholt.net>
2016-11-04 13:43:27 +0100

    Rydding i kode, LoadAtricles

--- Tungrocken/app/src/main/java/tungrocken/example/com/tungrocken/LoadArticles.java
+++ Tungrocken/app/src/main/java/tungrocken/example/com/tungrocken/LoadArticles.java
@@ -1,29 +1,24 @@
 package tungrocken.example.com.tungrocken;
 
-
-
 import android.os.AsyncTask;
-
-        import com.google.gson.Gson;
-        import com.google.gson.GsonBuilder;
-        import com.google.gson.JsonDeserializationContext;
-        import com.google.gson.JsonDeserializer;
-        import com.google.gson.JsonElement;
-        import com.google.gson.JsonParseException;
-
-        import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.lang.reflect.Type;
-        import java.net.HttpURLConnection;
-        import java.net.URL;
-        import java.text.ParseException;
-        import java.text.SimpleDateFormat;
-        import java.util.ArrayList;
-        import java.util.Arrays;
-        import java.util.Date;
-        import java.util.List;
-
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 import tungrocken.example.com.tungrocken.domain.Article;
 
 
@@ -35,7 +30,6 @@ public class LoadArticles extends AsyncTask<String,Long,List<Article>> {
     static SimpleDateFormat DF      = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
     static SimpleDateFormat DFSHORT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
 
-
     interface Callback {
         void update(List<Article> articles);
     }
@@ -43,7 +37,7 @@ public class LoadArticles extends AsyncTask<String,Long,List<Article>> {
     Callback callback;
 
     public LoadArticles(Callback callback) {
-        this.callback =callback;
+        this.callback = callback;
     }
 
     @Override
@@ -83,11 +77,8 @@ public class LoadArticles extends AsyncTask<String,Long,List<Article>> {
         return result;
     }
 
-
     @Override
     protected void onPostExecute(List<Article> articles) {
         callback.update(articles);
     }
-
-
 }
