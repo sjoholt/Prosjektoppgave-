a8893ed0716272cc7305bb8f314161d8e1d32f99
ghp91 <gaaute@hotmail.com>
2016-11-08 12:37:37 +0100

    prøving og feiling i utubetest

--- youtubetestapi/app/src/main/java/com/example/gautelokal/youtubetestapi/MainActivity.java
+++ youtubetestapi/app/src/main/java/com/example/gautelokal/youtubetestapi/MainActivity.java
@@ -1,48 +1,24 @@
 package com.example.gautelokal.youtubetestapi;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 
-import com.google.android.youtube.player.YouTubeBaseActivity;
-import com.google.android.youtube.player.YouTubeInitializationResult;
-import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerView;
 
 
-public class MainActivity extends YouTubeBaseActivity {
+public class MainActivity {
 
-    Button b;
-    private YouTubePlayerView youTubePlayerView;
-    private YouTubePlayer.OnInitializedListener onInitializedListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_view);
-        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
-            @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-            youTubePlayer.loadVideo("fBYVlFXsEME");
-            }
 
-            @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
 
-            }
-        };
 
-        b = (Button) findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                youTubePlayerView.initialize("AIzaSyC3BB6nhsBUlPGCJNRLSqCPg8vgr65Lqqk",onInitializedListener);
-            }
-        });
-    }
+
+
 
 
 
