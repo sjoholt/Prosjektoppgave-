5b4ae07d96259a0837cfc772724f4d3e11db0aff
sjoholt <sindre@sjoholt.net>
2016-11-22 14:10:09 +0100

    Ryddet på MainActivity

--- Tungrocken/app/src/main/java/tungrocken/example/com/tungrocken/MainActivity.java
+++ Tungrocken/app/src/main/java/tungrocken/example/com/tungrocken/MainActivity.java
@@ -33,51 +33,5 @@ public class MainActivity extends AppCompatActivity {
             Intent d = new Intent(MainActivity.this, HomeActivity.class);
             startActivity(d);
         }
-
-        // Oppsett av toolbar (uten logoclicklistener)
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        toolbar.setNavigationIcon(R.drawable.toolbarlogo);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        MenuItem menuItem = menu.findItem(R.id.app_bar_search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
-        SearchManager searchManager = (SearchManager)getSystemService(Context.SEARCH_SERVICE);
-        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
-        searchView.setSearchableInfo(searchableInfo);
-
-
-
-        return true;
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        // Håndtering av visning og klikk på hamburgermeny
-        HamburgerMenu hm = new HamburgerMenu();
-        Intent i = hm.getHamburgerMenu(id, this.getApplicationContext());
-        if(i != null) {
-            startActivity(i);
-            return true;
-        }
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
